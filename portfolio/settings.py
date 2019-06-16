b094bb6abd29d9fdb3fff695fa250ee66f09e6bd
camilo <johncamilocervantes@gmail.com>
2019-06-16 00:22:36 -0500

    -

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfolio_db',
         'USER': 'camilo',  #postgres
-        'PASSWORD': '####',
+        'PASSWORD': 'psyduck321',
         'HOST': 'localhost',
         'PORT': '5432'
     }
