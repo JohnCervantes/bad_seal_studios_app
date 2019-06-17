9c0d877676e7fcb7353d0c8745286fea3c7be9d2
camilo <johncamilocervantes@gmail.com>
2019-06-17 14:40:48 -0500

    -

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -82,8 +82,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'portfolio_db',
-        'USER': os.environ.get('bs_db_user'),  #postgres or camilo
-        'PASSWORD': os.environ.get('bs_db_password'), #os.environ['bs_password']
+        'USER': os.environ.get('BS_DB_USER'),  #postgres or camilo
+        'PASSWORD': os.environ.get('BS_DB_PASSWORD'), #os.environ['bs_password']
         'HOST': 'localhost',
         'PORT': '' #5432
     }
