4a7cc716e69d4af0fde379b3b0dadb2b07c62e23
camilo <johncamilocervantes@gmail.com>
2019-06-18 10:31:28 -0500

    -

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,21 @@ from django.shortcuts import render, get_object_or_404
 from .models import Blog
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from bs4 import BeautifulSoup
+import requests
 
+source = requests.get(
+    'https://myanimelist.net/anime/34134/One_Punch_Man_2nd_Season').text
+soup = BeautifulSoup(source, 'lxml')
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About', 'latest_news': Blog.objects.all().order_by('-pub_date').values('title','id')[0:3]})
+    source = requests.get(
+        'https://myanimelist.net/anime/34134/One_Punch_Man_2nd_Season').text
+
+
+soup = BeautifulSoup(source, 'lxml')
+return render(request, 'blog/about.html', {'title': 'About', 'latest_news': Blog.objects.all().order_by('-pub_date').values('title', 'id')[0:3]})
 
 
 class PostListViews(ListView):
@@ -17,25 +27,32 @@ class PostListViews(ListView):
     # ascending is ['date_posted'] . descending is ['-date_posted']
     ordering = ['-pub_date']
     paginate_by = 5
-
+    source = requests.get(
+        'https://leetcode.com/vocalists/').text
+    soup = BeautifulSoup(source, 'lxml')
+    match = []
+    for x in soup.find_all("span", {"class": "badge progress-bar-success"}):
+        self.match.append[x]
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Blog'
-        context['latest_news'] = self.model.objects.all().order_by('-pub_date').values('title','id')[0:3]
+        context['latest_news'] = self.model.objects.all().order_by(
+            '-pub_date').values('title', 'id')[0:3]
+        context['leet'] = self.match[1]
         return context
-
 
 
 class PostDetailViews(DetailView):
     model = Blog
     template_name = 'blog/details.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = Blog.objects.get(id=self.kwargs['pk']).title
-        context['latest_news'] = self.model.objects.all().order_by('-pub_date').values('title','id')[0:3]
+        context['latest_news'] = self.model.objects.all().order_by(
+            '-pub_date').values('title', 'id')[0:3]
         return context
-    
 
 
 class PostCreateViews(LoginRequiredMixin, CreateView):
@@ -46,6 +63,7 @@ class PostCreateViews(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Create a Post'
@@ -63,6 +81,7 @@ class PostUpdateViews(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         else:
             return False
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Update a Post'
@@ -72,14 +91,15 @@ class PostUpdateViews(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
 class PostDeleteViews(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Blog
     success_url = '/'
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:
             return True
         else:
             return False
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Delete a Post'
         return context
-        
